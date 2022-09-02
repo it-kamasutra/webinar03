f9fa142122a0901099f26e2b90e78b652f768c58
Dimych <kuzyuberdin@gmail.com>
2022-09-02 22:42:49 +0400

    fix port; add start script;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {MongoClient} = require('mongodb');
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000; 
 
 const uri = "mongodb+srv://books-user:books-user111@myfirstcluster.xi3rgwx.mongodb.net/?retryWrites=true&w=majority"
 
